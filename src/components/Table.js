4e1788c1d1bb8b026105082a9df97363a0ed5111
shiva kumar gattu <gshiva0018@gmail.com>
2025-01-22 16:23:02 +0530

    table components

---
+++ src/components/Table.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Table.css"
+const Table = () => {
+  return (
+    <div className='mainContainer'>
+        <div>
+            <h1>Table 1</h1>
+            
+        </div>
+        <div>
+            <h1>Table 2</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Table
